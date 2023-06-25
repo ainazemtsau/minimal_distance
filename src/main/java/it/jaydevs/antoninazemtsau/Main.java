3f0ce816afdac46484f6cc4d8df88ddeb71bcc60
antoninozemcev <malah@saldoapps.com>
2023-06-25 14:56:37 +0400

    check number of arguments in main method

--- src/main/java/it/jaydevs/antoninazemtsau/Main.java
+++ src/main/java/it/jaydevs/antoninazemtsau/Main.java
@@ -6,6 +6,9 @@ import it.jaydevs.antoninazemtsau.service.MinimalDistanceServiceImpl;
 public class Main {
     private static MinimalDistanceService minimalDistanceService = new MinimalDistanceServiceImpl();
     public static void main(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Please provide two words as arguments");
+        }
         minimalDistanceService.minimalDistance(args[0], args[1]);
     }
 }
